7b717ded09f23875c85acc3ae7c36c106bd9143c
Rochelle Hickey <rochellehickey@gmail.com>
2020-01-06 15:25:34 -0500

    Updated for screensize 768 and JS changing logos

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const imageLogo = document.querySelector('.julia-gilstein-logo');
+
+function changeImage() {
+  console.log('1');
+  if (document.body.clientWidth < 768) {
+    console.log('2');
+      imageLogo.src = "img/horizontal-logo.png";
+  } else if (document.body.clientWidth >= 768) {
+      console.log('3');
+      imageLogo.src = "img/stacked-logo.png";
+  }
+};
+
+changeImage();
